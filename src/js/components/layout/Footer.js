69db8afbfef83659cbabb695e2bffd56789f3db6
KEITH L ROBINSON <krobnson@gmail.com>
2018-04-12 11:03:07 -0500

    next level

--- src/js/components/layout/Footer.js
+++ src/js/components/layout/Footer.js
@@ -2,6 +2,10 @@ import React from "react";
 
 export default class Footer extends React.Component {
   render() {
+    const footerStyles = {
+      marginTop: "30px",
+    };
+    
     return (
       <footer>
       <div class="row">
