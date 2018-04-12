eb4ec0a1e173b394d2137f8cd54b7563ba3a8a34
KEITH L ROBINSON <krobnson@gmail.com>
2018-04-12 08:30:44 -0500

    simpleReact app first commit

---
+++ src/js/pages/Archives.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Article from "../components/Article";
+
+export default class Archives extends React.Component {
+  render() {
+    const { query } = this.props.location;
+    const { params } = this.props;
+    const { article } = params;
+    const { date, filter } = query;
+
+    const Articles = [
+      "Javascript",
+      "HTML",
+      "CSS",
+      "React",
+      "Ruby on Rails",
+      "git",
+      "dataBases",
+      "concepts",
+    ].map((title, i) => <Article key={i} title={title}/> );
+
+    return (
+      <div>
+        <h1>Archives</h1>
+        article: {article}, date: {date}, filter: {filter}
+        <div class="row">{Articles}</div>
+      </div>
+    );
+  }
+}
